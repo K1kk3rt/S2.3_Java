0d26a27ab3d5f363d0b40ba80de2f20c8a21b8f0
K1kk3rt <eugenie-heijn@hotmail.com>
2019-03-06 14:48:11 +0100

    fixed layout shit

---
+++ 3.1 Wekker/src/Model.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Model implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == "BPlus") {
+			
+		}
+	}
+
+	
+}
--- 3.1 Wekker/src/View.java
+++ 3.1 Wekker/src/View.java
@@ -1,20 +1,31 @@
 import java.awt.Toolkit;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.*;
+import java.util.Observable;
 
 public class View extends JFrame{
-
+	JPanel P;
+	JPanel PBottom;
+	
+	JLabel LTitle;
+	JLabel LTimer;
+	
+	JButton BPlus;
+	JButton BMinus;
+	JButton BStart;
+	JButton BStop;
+	
+	
 	//construct
 	public View() {
 			
 		createWindow();
-		
-		createContent();
 	}
 	
 	private void createWindow() {
 		
-		this.setSize(400,400);
+		this.setSize(400,200);
 		
 		//set location of window in middle of screen
 		Toolkit tk = Toolkit.getDefaultToolkit();	
@@ -27,14 +38,44 @@ public class View extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Alarm clock");
 		
+		//panel
+		P = new JPanel(new BorderLayout());
+		
+		createContent();
+		
+		//add panel to window
+		add(P);
+		
 		setVisible(true);
 	}
 	
 	private void createContent() {
 		
-		JPanel p = new JPanel();
-		JLabel lTitle = new JLabel("I'm A clock");
+		//content for top
+		LTitle = new JLabel("I'm an alarm clock :)");
+		LTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		P.add(LTitle, BorderLayout.PAGE_START);
 		
-		p.add(lTitle);
+		//content for middle
+		BPlus = new JButton("+");
+		BMinus = new JButton("-");
+		BPlus.addActionListener(new Model());
+		
+		LTimer = new JLabel("I'm the timer!");
+		LTimer.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		P.add(LTimer, BorderLayout.CENTER);
+		P.add(BPlus, BorderLayout.LINE_END);
+		P.add(BMinus, BorderLayout.LINE_START);
+		
+		//content for bottom
+		PBottom = new JPanel();
+		BStart = new JButton("Start");
+		BStop = new JButton("Stop");
+		PBottom.add(BStop);
+		PBottom.add(BStart);
+		
+		P.add(PBottom, BorderLayout.PAGE_END);
+		
 	}
 }
